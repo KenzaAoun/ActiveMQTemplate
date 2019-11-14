f1806636000b1bcd9e25150dcaf9db23110dad59
KenzaAoun <57532302+KenzaAoun@users.noreply.github.com>
2019-11-14 12:10:19 +0100

    Version Réalisée en classe
     Reception de msg OK / Envoi de msg OK
    TP à poursuivre

--- src/main/java/receiver/MyReceiver.java
+++ src/main/java/receiver/MyReceiver.java
@@ -18,19 +18,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyReceiver {
 
 	public static void main(String[] args) {
-		try{
-			
+		try {
+
 			ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContextJMS.xml");
 			QueueConnectionFactory factory = (QueueConnectionFactory) applicationContext.getBean("connectionFactory");
-			
+
 			Queue queue = (Queue) applicationContext.getBean("queue");
-			
+			QueueConnection connection = null;
 			// Create a connection. See https://docs.oracle.com/javaee/7/api/javax/jms/package-summary.html
-			// Open a session	
-			// start the connection	
-			// Create a receive	
+			connection = factory.createQueueConnection();
+			// Open a session
+			QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			//Call of a class named 'SESSION' which have an object named 'AUTO_AKNOWLEGDGE'
+			// start the connection
+			connection.start();
+			// Create a receive
+			QueueReceiver receiver = session.createReceiver(queue);
 			// Receive the message
-			
+			Message m = receiver.receive();
+			System.out.println("Message is"+m);//display the message sent
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
